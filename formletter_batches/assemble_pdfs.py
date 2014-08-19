5c032cf3b2ca648bab5f0771177f7306085d00d7
aliyarahman <aliya.rahman@yahoo.com>
2014-08-18 22:22:31 -0400

    Added code for rearrnaging pdfs of form letters into district batches

---
+++ formletter_batches/assemble_pdfs.py
@@ -0,0 +1,39 @@
+# This is the pyPdf library. Install with: $ pip install pyPdf
+from pyPdf import PdfFileWriter, PdfFileReader
+
+# Generate a list of batchnames to loop through later
+batchnames = []
+total_batches = input("How many batches of pdfs do you have?: ")
+for i in range(1, total_batches+1):
+   batchnames.append("batch"+str(i)+".pdf")
+
+# Loop through HouseDistricts
+for i in range(1,100):
+      output = PdfFileWriter()
+      outputfilename = "HD"+str(i)+".pdf"
+      for b in batchnames:
+         input_pdf = PdfFileReader(file(b, "rb"))
+         for row in google_doc_dump:
+	     if row[0] == b:
+                if row[7]== i:
+	        output.addPage(input_pdf.getPage(int(row[1]))
+      outputStream = file(outputfilename, "wb")
+      output.write(outputStream)
+      outputStream.close()
+      print "There are %s form letters in %s " % (output.getNumPages(), outputfilename)
+
+
+# Loop through SenateDistricts
+for i in range(1,34):
+      output = PdfFileWriter()
+      outputfilename = "SD"+str(i)+".pdf"
+      for b in batchnames:
+         input_pdf = PdfFileReader(file(b, "rb"))
+         for row in google_doc_dump:
+        if row[0] == b:
+                if row[7]== i:
+           output.addPage(input_pdf.getPage(int(row[1]))
+      outputStream = file(outputfilename, "wb")
+      output.write(outputStream)
+      outputStream.close()
+      print "There are %s form letters in %s " % (output.getNumPages(), outputfilename)
