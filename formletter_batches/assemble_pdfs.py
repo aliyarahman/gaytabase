4abda51bfccfed0e2762bb484c9ea1b3014b466d
aliyarahman <aliya.rahman@yahoo.com>
2014-08-19 12:01:03 -0400

    Corrected house and senate ordering in bulk geocode script

--- formletter_batches/assemble_pdfs.py
+++ formletter_batches/assemble_pdfs.py
@@ -14,31 +14,33 @@ for i in range(1,100):
       outputfilename = "pdfs/HD"+str(i)+".pdf"
       for b in batchnames:
          input_pdf = PdfFileReader(file("pdfs/"+b, "rb"))
-         with open('csvs/google_doc_dump.csv', 'rb') as csvfile:
-	    google_doc_dump = csv.reader(csvfile, delimiter=',', quotechar='"')
-            for row in google_doc_dump:
-	       if row[0] == b:
-                  if row[7]== i:
-	             output.addPage(input_pdf.getPage(int(row[1]))
-      outputStream = file(outputfilename, "wb")
-      output.write(outputStream)
-      outputStream.close()
-      print "There are %s form letters in %s " % (output.getNumPages(), outputfilename)
+         with open('csvs/district_data.csv', 'rb') as csvfile:
+            district_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in district_reader:
+               if row[0] == b:
+                  if row[11]== str(i):
+                    output.addPage(input_pdf.getPage(int(row[1])))
+               if output.getNumPages() >0:
+                  outputStream = file(outputfilename, "wb")
+                  output.write(outputStream)
+                  outputStream.close()
+                  print "There are %s form letters in %s " % (output.getNumPages(), outputfilename)
 
-
+'''
 # Loop through SenateDistricts
 for i in range(1,34):
       output = PdfFileWriter()
       outputfilename = "pdfs/SD"+str(i)+".pdf"
       for b in batchnames:
          input_pdf = PdfFileReader(file("pdfs/"+b, "rb"))
-         with open('csvs/google_doc_dump.csv', 'rb') as csvfile:
+         with open('csvs/district_data.csv', 'rb') as csvfile:
             google_doc_dump = csv.reader(csvfile, delimiter=',', quotechar='"')
             for row in google_doc_dump:
                if row[0] == b:
-                  if row[7]== i:
-                     output.addPage(input_pdf.getPage(int(row[1]))
+                  if row[13]== str(i):
+                     output.addPage(input_pdf.getPage(int(row[1])))
       outputStream = file(outputfilename, "wb")
       output.write(outputStream)
       outputStream.close()
       print "There are %s form letters in %s " % (output.getNumPages(), outputfilename)
+'''
--- formletter_batches/bulk_find_districts_from_addresses.py
+++ formletter_batches/bulk_find_districts_from_addresses.py
@@ -4,36 +4,52 @@ import pprint
 from api_keys import *
 
 # Open the Google doc csv
-with open('csvs/google_doc_dump.csv', 'rb') as csvfile:
+with open('csvs/google_doc_dump.csv', 'rb') as csvfile, open('csvs/geocoded_letter_data.csv', 'wb') as outfile:
       google_doc_dump_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+      geocodes_writer = csv.writer(outfile, delimiter=',', quotechar='"')
       for index, row in enumerate(google_doc_dump_reader):
        if index>0:
-        if row[8] != "":
-         print "Not in Ohio"
-        else:
          # Geocode each address with Google Maps API
          address = row[4]+", "+row[5]+", OH "+row[6]
          url = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+GOOGLE_GEOCODE_API_KEY
-         r = requests.get(url)
-         results = r.json()		# Save your answer to results
-         lat = results['results'][0]['geometry']['location']['lat']
-         lng = results['results'][0]['geometry']['location']['lng']
+         if row[8] != "":
+            lat= "None"
+            lng= "None"
+         else:
+            r = requests.get(url)
+            results = r.json()            # Save your answer to results
+            lat = results['results'][0]['geometry']['location']['lat']
+            lng = results['results'][0]['geometry']['location']['lng']
+         geocodes_writer.writerow([row[0],row[1], row[2],row[3],row[4], row[5], row[6], row[7], row[8], lat, lng])
          print lat, lng
-         # If there is no match, add batch, page, name, and address to a separate 'not geocoded' file
-         # Otherwise, write latitude and longitude coordinates to a new .csv in row[7] and row[8] respectively
-         # NEED TO FINISH WRITE PORTION
 
-#with open('csvs/geocoded_letter_data.csv', 'rb') as csvfile:
-#     geocoded_letter_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-#      for row in geocoded_letter_reader:
-#         lat = row[7]
-#         lng = row[8]
+with open('csvs/geocoded_letter_data.csv', 'rb') as csvfile, open('csvs/district_data.csv', 'wb') as districtsfile:
+     geocoded_letter_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+     district_writer = csv.writer(districtsfile, delimiter=',', quotechar='"')
+     for row in geocoded_letter_reader:
+         lat = row[9]
+         lng = row[10]
+         if lat == "None":
+            HD="None"
+            SD="None"
+            Rep="None"
+            Sen="None"
          # Make the Sunlight API call for each lat/long pair
-#         url = "http://openstates.org/api/v1//legislators/geo/?lat="+str(lat)+"&long="+str(lng)+"&apikey="+SUNLIGHT_API_KEY
-#         r = requests.get(url)
-#         state_legislators = r.json()
-         # If there is no match, add batch, page, name, and address to a separate 'not found' file
-         # Otherwise, write the HD, SD, legislator information to each cell
-         # Write the new csv and call it 'google_doc_dump.csv'
-         # NEED TO FINISH WRITE PORTION
+         else:
+            url = "http://openstates.org/api/v1//legislators/geo/?lat="+str(lat)+"&long="+str(lng)+"&apikey="+SUNLIGHT_API_KEY
+            r = requests.get(url)
+            results = r.json()
+            if results[0]['chamber'] == 'upper':
+               HD = results[1]['district']
+               Rep = results[1]['last_name']
+               SD = results[0]['district']
+               Sen = results[0]['last_name']
+            else:
+               HD = results[0]['district']
+               Rep = results[0]['last_name']
+               SD = results[1]['district']
+               Sen = results[1]['last_name']
+            print HD, Rep, SD, Sen
+         district_writer.writerow([row[0],row[1], row[2],row[3],row[4], row[5], row[6], row[7], row[8], HD, Rep, SD, Sen])
 
+#Need to add blocks for how to deal with bad addresses and compile a file
